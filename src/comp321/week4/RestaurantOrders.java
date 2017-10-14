fbfd1ee65a286e510922ae7f216d1cc5c65b95fb
Yann Vonlanthen <yann.vonlanthen@epfl.ch>
2017-10-13 22:35:28 -0400

    week 4 final version

--- src/comp321/week4/RestaurantOrders.java
+++ src/comp321/week4/RestaurantOrders.java
@@ -7,6 +7,7 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
 
 /**
@@ -26,11 +27,9 @@ public class RestaurantOrders {
 
     public static void main(String[] args) throws FileNotFoundException {
         // Uncomment this to test on a local file
-        /*
-        File initialFile = new File("src/comp321/week4/orders-1.txt");
-        InputStream targetStream = new FileInputStream(initialFile);
-        Kattio io = new Kattio(targetStream, System.out);
-        */
+//        File initialFile = new File("src/comp321/week4/orders-2.in");
+//        InputStream targetStream = new FileInputStream(initialFile);
+//        Kattio io = new Kattio(targetStream, System.out);
         // Use Kattio for I/O as suggested in tutorial
         Kattio io = new Kattio(System.in, System.out);
 
@@ -44,20 +43,28 @@ public class RestaurantOrders {
 
         for (int i = 0; i < n; i++) {
             int item = io.getInt();
-            items.add(item);
+                items.add(item);
         }
-        Collections.sort(items);
 
-        // Initialize DP-Array
+        Collections.sort(items);
         
+        // Initialize DP-Array
         int[][] r = new int[n][maxOrder]; // FIXME allocate smarter dont use max order
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < maxOrder; j++) {
                 r[i][j] = Integer.MIN_VALUE;
             }
         }
-
         
+        // Initialize Solution Array
+        int[][] m = new int[n][maxOrder]; // FIXME allocate smarter dont use max order
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < maxOrder; j++) {
+                m[i][j] = Integer.MIN_VALUE;
+            }
+        }
+        
+
         // Get number of orders placed
         int orders = io.getInt();
 
@@ -66,16 +73,17 @@ public class RestaurantOrders {
 
         for (int i = 0; i < orders; i++) {
             int orderValue = io.getInt();
-            int x = memoized_orders_aux(r, orderValue, n-1, items);
+            int x = memoized_orders_aux(r, orderValue, n-1, items, m);
 
             // Output correct values
             if (x == Integer.MAX_VALUE) {
                 io.println("Ambiguous");
             } else if (x == 0) {
                 io.println("Impossible");
+            } else if (x == 1){
+                tracebackItems(io, r, orderValue, n-1, items, m);
             } else {
-                tracebackItems(io, r, orderValue, n-1, items);
-                io.println();
+                throw new Error();
             }
         }
 
@@ -84,21 +92,15 @@ public class RestaurantOrders {
     }
 
     public static int memoized_orders_aux(int[][] r, int order,
-            int itemIndex, ArrayList<Integer> items) {
+            int itemIndex, ArrayList<Integer> items, int[][] m) {
        
         if (order == 0){
-            r[itemIndex][order] = 1;
             return 1; //aim reached            
         }
         if (order < 0 || itemIndex < 0) {
             return 0; //overshoot, no more items left            
         }
-        if (itemIndex == 0 && order < 0) {
-            return 0;
-        }
-        if (order > maxOrder) {
-            throw new Error("unexpected!!");    //remove this case
-        }
+        
         int x = r[itemIndex][order];
         if (x > Integer.MIN_VALUE) {
             return x; // already computed
@@ -106,59 +108,63 @@ public class RestaurantOrders {
 
         // Here the magic happens
 
-        int dontBuyMostExpensive = memoized_orders_aux(r, order, itemIndex - 1, items);
-        int buyMostExpensive = memoized_orders_aux(r, order - items.get(itemIndex), itemIndex, items);
+        int dontBuyMostExpensive = memoized_orders_aux(r, order, itemIndex - 1, items, m);
+        int buyMostExpensive = memoized_orders_aux(r, order - items.get(itemIndex), itemIndex, items, m);
 
         // FIXME rearrange test cases
 
         // --- Case 1 : problem has more than two solutions ---
-        if (buyMostExpensive == Integer.MAX_VALUE
-                || dontBuyMostExpensive == Integer.MAX_VALUE
-                || ((buyMostExpensive > 0 || buyMostExpensive == -1) 
-                        && (dontBuyMostExpensive > 0 || dontBuyMostExpensive == -1))) {
-            r[itemIndex][order] = Integer.MAX_VALUE;
+        if (buyMostExpensive > 1
+                || dontBuyMostExpensive > 1
+                || (buyMostExpensive == 1 && dontBuyMostExpensive == 1 )) {
+            
+            r[itemIndex][order] = Integer.MAX_VALUE;    // store max value to indicate ambiguous
+            m[itemIndex][order] = Integer.MAX_VALUE;    // store max value to indicate ambiguous
+            
             return Integer.MAX_VALUE;
         }
 
         // --- Case 2: one solution found ---
-        else if (buyMostExpensive > 0 || buyMostExpensive == -1) {
-            int i = items.get(itemIndex);
-            r[itemIndex][order] = i;
-            return i;
+        else if (buyMostExpensive == 1) {
+            m[itemIndex][order] = 1;    // store index of bought item
+            r[itemIndex][order] = 1;    // store price of bought item
+            return 1;
         }
         
         // --- Case 3: one solution found ---
-        else if (dontBuyMostExpensive > 0 || dontBuyMostExpensive == -1) {
-            r[itemIndex][order] = -1;
-            return -1;
+        else if (dontBuyMostExpensive == 1) {
+            m[itemIndex][order] = -1;  // store -1 to indicate an item was skipped
+            r[itemIndex][order] = 1;
+            return 1;
         }
         
         // --- Case 4: none of above, no solution ---
-        r[itemIndex][order] = 0;
+        r[itemIndex][order] = 0;    // store 0 to indicate no solution found
+        m[itemIndex][order] = 0;    // store 0 to indicate no solution found
         return 0;
     }
 
-    public static void tracebackItems(Kattio io, int[][] r, int value, int itemIndex, ArrayList<Integer> items) {
+    public static void tracebackItems(Kattio io, int[][] r, int value, int itemIndex, ArrayList<Integer> items, int[][] m) {
         int moneyLeft = value;
         int item = itemIndex;
         
         LinkedList<Integer> ordered = new LinkedList<>();
 
         while (moneyLeft != 0) {
-            int x = r[item][moneyLeft];
+            int x = m[item][moneyLeft];
             if (x == -1){
                 item--;
-            } else {
-                int i = items.get(item);
-                ordered.add(item + 1);
-                moneyLeft -= i;
+            } else if (x == 1){
+                ordered.add(item+1);
+                moneyLeft -= items.get(item);
             }
         }
-        Collections.reverse(ordered);
+        Collections.sort(ordered);
+        
         for (int i = 0; i < ordered.size()-1; i++){
             io.print(ordered.get(i));
             io.print(" ");
         }
-        io.print(ordered.getLast());
+        io.println(ordered.get(ordered.size()-1));
     }
 }
---
+++ src/comp321/week4/RestaurantOrdersBottomUp.java
@@ -0,0 +1,166 @@
+package comp321.week4;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+
+import comp321.week4.Kattio;
+
+/**
+ * Restaurant Orders. Assignment 4. (https://open.kattis.com/problems/orders)
+ * 
+ * DP-Algorithm implementation
+ * 
+ * @author Yann Vonlanthen (260808862)
+ *
+ */
+public class RestaurantOrdersBottomUp {
+
+    /*
+     * Global variables
+     */
+    public static int maxOrder = 30000;
+
+    public static void main(String[] args) throws FileNotFoundException {
+        
+        // Uncomment this to test on a local file
+        // File initialFile = new File("src/comp321/week4/orders-1.txt");
+        // InputStream targetStream = new FileInputStream(initialFile);
+        // Kattio io = new Kattio(targetStream, System.out);
+        
+        // Use Kattio for I/O as suggested in tutorial
+        Kattio io = new Kattio(System.in, System.out);
+
+        // Get the numbers of items on menu
+        int n = io.getInt();
+
+        // Read all menu items
+        ArrayList<Integer> items = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int item = io.getInt();
+                items.add(item);
+        }
+
+        // Create DP-Array
+        int[][] r = new int[n][maxOrder + 1];
+
+        // When 0 money left there is one solution: not ordering anything!
+        for (int i = 0; i < n; i++)
+            r[i][0] = 1;
+        
+        
+        // Fill out 2D-array, Note: I believe a 1D array could also be sufficient
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < maxOrder + 1; j++) {
+
+                int dontBuy;
+                if (i == 0){
+                    dontBuy = -1;
+                }else {
+                    dontBuy = r[i-1][j];                    
+                }
+                
+                int buy;
+                if (j - items.get(i) < 0){
+                    buy = -1;                    
+                }
+                else {
+                    buy = r[i][j-items.get(i)];                    
+                }
+                
+                // --- case 1: ambiguous solution 
+                if ((buy >= 0 && dontBuy >= 0) ||
+                     buy == Integer.MAX_VALUE ||
+                     dontBuy == Integer.MAX_VALUE) {
+                    r[i][j] = Integer.MAX_VALUE;
+                }
+                
+                // --- case 2: there is a solution by buying item i
+                else if (buy >= 0) {
+                    r[i][j] = 1;
+                }
+                
+                // --- case 3: there is a solution by not buying item i
+                else if (dontBuy >= 0) {
+                    r[i][j] = 0;
+                }
+                
+                // --- case 4: there is no solution
+                else if (dontBuy < 0 && buy < 0){
+                    r[i][j] = -1;
+                }
+                
+                // --- unexpected (can be deleted)
+                else {
+                    throw new Error();
+                }
+            }
+        }
+        
+        
+        // Get number of orders placed
+        int orders = io.getInt();
+
+        // Iterate through all orders and find correct answer
+        for (int i = 0; i < orders; i++) {
+            int orderValue = io.getInt();
+            int x = r[n-1][orderValue];
+
+            // Output correct values
+            if (x == Integer.MAX_VALUE) {
+                io.println("Ambiguous");
+            } else if (x == -1) {
+                io.println("Impossible");
+            } else if (x >= 0){
+                tracebackItems(io, r, orderValue, items);
+            } else {
+                throw new Error();
+            }
+        }
+        
+        // Close I/O stream and terminate
+        io.close();
+    }
+
+    
+    /**
+     * This method prints a solution to given io interface.
+     */
+    public static void tracebackItems(Kattio io, int[][] r, int order, ArrayList<Integer> items) {
+        int i = items.size() - 1;
+        int j = order;
+        ArrayList<Integer> solution = new ArrayList<>();
+        
+        // go through array and save items that were ordered
+        while(j > 0){
+            int x = r[i][j];
+            if(x == 0){
+                i -= 1;
+            } 
+            else if(x == 1){
+                int price = items.get(i);
+                solution.add(i + 1);
+                j -= price;
+            } else {
+                throw new Error();
+            }
+        }
+
+        // sort and then print solution with spaces
+        Collections.sort(solution);
+        StringBuilder sb = new StringBuilder();
+        for (int x : solution) {
+            sb.append(x).append(" ");
+        }
+
+        String result = sb.deleteCharAt(sb.length() - 1).toString();
+        io.println(result);
+    }
+}
