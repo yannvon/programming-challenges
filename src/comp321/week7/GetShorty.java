b1e673e6f81acc8f9595bcb7d3fffca563515fc0
Yann Vonlanthen <yann.vonlanthen@epfl.ch>
2017-11-06 21:58:05 -0400

    week7 submitted version, many more improvements could be made

--- src/comp321/week7/GetShorty.java
+++ src/comp321/week7/GetShorty.java
@@ -5,13 +5,10 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.text.DecimalFormat;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 
 import comp321.week7.Kattio;
-import sun.misc.Queue;
 
 /**
  * Solution to the GetShorty Problem on Kattis.
@@ -19,8 +16,8 @@ import sun.misc.Queue;
  * 
  * Use of shortest path on a graph. (Dijkstra's Algorithm)
  * 
- * Make use of smart adjacency list for nice speedup!
- * TEST with normal adjacency list ! TODO
+ * TODO Make use of smart adjacency list for nice speedup! 
+ * Done first with normal adjacency list.
  * 
  * @author Yann Vonlanthen (260808862)
  *
@@ -29,70 +26,91 @@ public class GetShorty {
 
     private static DecimalFormat df4 = new DecimalFormat("#0.0000");
 
-
     public static void main(String[] args) throws FileNotFoundException {
 
         // Uncomment this to test on a local file
-//        File initialFile = new File("src/comp321/week7/getshorty.in");
-//        InputStream targetStream = new FileInputStream(initialFile);
-//        Kattio io = new Kattio(targetStream, System.out);
+        // File initialFile = new File("src/comp321/week7/getshorty.in");
+        // InputStream targetStream = new FileInputStream(initialFile);
+        // Kattio io = new Kattio(targetStream, System.out);
 
         // Use Kattio for I/O as suggested in tutorial
-         Kattio io = new Kattio(System.in, System.out);
+        Kattio io = new Kattio(System.in, System.out);
 
-        // Iterate through all problem instances (max 20)
 
         // Read input
         int vertices = io.getInt(); // intersections
-        int edges = io.getInt();    // corridors
+        int edges = io.getInt(); // corridors
         int startNode = 0;
         int targetNode = vertices - 1;
 
+        // Iterate through all problem instances (max 20)
         while (edges > 0 || vertices > 0) {
 
             // Read all vertices input and store in adjacency list (lec 6)
-            //HashMap<Integer, Integer> length = new HashMap<>(); //length for given edge id
-            
-            // --- Method 1: Linked list
-            
-            List<Pair>[] successors = new List[vertices];
-            
+
+            // --- Method 1: Linked List
+            List<Pair<Integer, Double>>[] successors = new List[vertices];
             for (int i = 0; i < vertices; i++) {
-                successors[i] = new LinkedList<Pair>();
+                successors[i] = new LinkedList<Pair<Integer, Double>>();
             }
             
-            for(int e = 0; e < edges; e++){
+            // --- Method 2: Lecture Trick
+            // Pair<Integer, Integer>[] E = new Pair[2*edges]; //the pair represents (to, nextID)
+            // int[] LE = new int[vertices];
+            // for(int i = 0; i < 2*edges; i++)
+            // E[i] = new Pair<Integer, Integer>(0,0);
+            // for(int i = 0; i < vertices; i++)
+            // LE[i] = -1;
+            // double[] lengths = new double[2*edges];
+            
+            
+            for (int e = 0; e < edges; e++) {
                 int start = io.getInt();
                 int end = io.getInt();
-                double l = -1 * Math.log(io.getDouble());  //TODO Trick that solves this problem
                 
-                successors[start].add(new Pair(end, l));    //don't add edge 
-                successors[end].add(new Pair(start, l)); //undirected graph!
+                // Trick that solves this problem, taking log allows us 
+                // to reduce the problem to a shortest path search.
+                double l = -1 * Math.log(io.getDouble()); 
+
+                // We have an undirected graph so we add the edge in both directions
+                
+                // --- Method 1 ---
+                successors[start].add(new Pair<Integer, Double>(end, l)); 
+                successors[end].add(new Pair<Integer, Double>(start, l));
+                
+                // --- Method 2 ---
+                // E[2*e] = new Pair(end, LE[start]);
+                // LE[start] = 2*e;
+                // E[2*e + 1] = new Pair(start, LE[end]);
+                // LE[end] = 2*e + 1;
+                // lengths[2*e] = l;
+                // lengths[2*e + 1] = l;
             }
 
             // --- ALGORITHM 1 --- Dijkstra's shortest path Algorithm no improvements
-            
-            //--- create array for the distances ---
+
+            // --- create array for the distances ---
             double[] distance = new double[vertices];
-            
-            //--- create array fort the best Predecessor of each vertex ---
-            //int[] bestPredecessor = new int[vertices];
-            
-            //--- fill both arrays: distance[] with infinity and bestPredecessor[] with -1 ---
-            for (int i = 0; i < distance.length; ++i){
+
+            // --- create array fort the best Predecessor of each vertex ---
+            // int[] bestPredecessor = new int[vertices];
+
+            // --- fill both arrays: distance[] with infinity and
+            // bestPredecessor[] with -1 ---
+            for (int i = 0; i < distance.length; ++i) {
                 distance[i] = Double.POSITIVE_INFINITY;
-                // bestPredecessor[i] = -1; 
+                // bestPredecessor[i] = -1;
             }
-            
-            //--- Dijkstra Algorithm ---
+
+            // --- Dijkstra Algorithm ---
             distance[startNode] = 0;
             boolean modified = true;
-            while (modified){
+            while (modified) {
                 modified = false;
-                for (int v = 0;  v < successors.length; ++v){                       
-                    if (!Double.isInfinite(distance[v])){                //suggested optimisation
-                        for (Pair<Integer, Double> p : successors[v]){
-                            if (distance[p._1()] > distance[v] + p._2()){ 
+                for (int v = 0; v < vertices; ++v) {
+                    if (!Double.isInfinite(distance[v])) {
+                        for (Pair<Integer, Double> p : successors[v]) { //method 1: successors[v]
+                            if (distance[p._1()] > distance[v] + p._2()) {
                                 distance[p._1()] = distance[v] + p._2();
                                 // bestPredecessor[p._1()] = v;
                                 modified = true;
@@ -102,34 +120,45 @@ public class GetShorty {
                 }
             }
             
-            double dist = Math.pow(Math.E, -distance[targetNode]);
+            
+            // --- ALGORITHM 2 --- Dijkstra's with queue
+            // TODO actually more complicated and speedup is not needed here
             
             
-            io.println(df4.format(dist));
-            
-            
-            // set new problem
+            // get back the factor by which he gets shorter from log distance
+            double f = Math.pow(Math.E, -distance[targetNode]);
+
+            io.println(df4.format(f));
+
+            // set new problem instance
             vertices = io.getInt();
             edges = io.getInt();
             targetNode = vertices - 1;
         }
-        
+
         // Close I/O stream and terminate
         io.close();
-
     }
-    
-    public static class Pair<A, B> {
-        private A a;
-        private B b;
-        
-        public Pair(A a, B b){
+
+    /**
+     *  
+     * Quick Helper class.
+     * Represents a Pair.
+     * Should be immutable.
+     */
+    public static final class Pair<A, B> {
+        private final A a;
+        private final B b;
+
+        public Pair(A a, B b) {
             this.a = a;
             this.b = b;
         }
+
         public A _1() {
             return a;
         }
+
         public B _2() {
             return b;
         }
